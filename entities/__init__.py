f096f98ab86898a1b6554fa9cf4cc5fc76c94e4d
gbascans <gbascansc@gmail.com>
2024-11-08 19:43:26 -0300

    gremio iniciación

---
+++ entities/__init__.py
@@ -0,0 +1,6 @@
+from .guerrero import Guerrero
+from .mago import Mago
+from .mascota import Mascota
+from .mision_grupal import MisionGrupal
+from .mision_individual import MisionIndividual
+from .ranger import Ranger
---
+++ gremio.py
@@ -0,0 +1,43 @@
+from entities import Guerrero, Mago, Mascota, MisionGrupal, MisionIndividual, Ranger
+
+
+class Gremio:
+    def __init__(self):
+        self.__misiones = []
+        self.__aventureros = []
+
+    @property
+    def misiones(self):
+        return self.__misiones
+
+    @property
+    def aventureros(self):
+        return self.__aventureros
+
+    def registrar_aventurero(self):
+        pass
+
+    def registrar_mision(self):
+        pass
+
+    def realizar_mision(self):
+        pass
+
+    def top_10_misiones_resueltas(self):
+        pass
+
+    def top_10_habilidad(self):
+        pass
+
+    def top_10_misiones_recompensa(self):
+        pass
+
+    def aventureros_por_clase(self):
+        pass
+
+
+if __name__ == "__main__":
+    try:
+        pass
+    except:
+        pass
