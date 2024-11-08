3d0ec467b3aa1539b3f167ccd7ab6a53493624bf
um-garamburu <garamburu@correo.um.edu.uy>
2024-11-08 19:08:34 -0300

    Update Aventurero

--- entities/aventurero.py
+++ entities/aventurero.py
@@ -1,36 +1,26 @@
 from abc import ABC
 
-<<<<<<< Updated upstream
 class aventurero(ABC):
-    def __init__(self, nombre:str, id:int, ptos_habilidad: int, experiencia: int, dinero: float)
-=======
+    def __init__(self, nombre:str, id:int, ptos_habilidad: int, experiencia: int, dinero: float):
+        self.__nombre = nombre
+        self.__id = id
+        self.__ptos_habilidad = ptos_habilidad
+        self.__experiencia = experiencia
+        self.__dinero = dinero
 
-class aventurero(ABC):
-    def __init__(self, nombre: str, id: int, ptos_habilidad: int, experiencia: int, dinero: float):
->>>>>>> Stashed changes
-        self._nombre = nombre
-        self._id = id
-        self._ptos_habilidad = ptos_habilidad
-        self._experiencia = experiencia
-        self._dinero = dinero
-
-<<<<<<< Updated upstream
         @property
         def nombre(self):
-            return self._nombre
+            return self.__nombre
         @property
         def id(self):
-            return self._id
+            return self.__id
         @property
         def ptos_habilidad(self):
-            return self._ptos_habilidad
+            return self.__ptos_habilidad
         @property
         def experiencia(self):
-            return self._experiencia
+            return self.__experiencia
         @property
         def dinero(self):
-            return self._dinero
+            return self.__dinero
                 
-=======
-        
->>>>>>> Stashed changes
